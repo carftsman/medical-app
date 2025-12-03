b371e1cc8c695f3e8f6e01bf3163955fb56c9cd9
jahnavikotti <jahnavikotti79@gmail.com>
2025-12-03 11:47:36 +0530

    OnboardingScreens code

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// Import your screens
-//import HomeScreen from '../screens/HomeScreen';
-//import LoginScreen from '../screens/LoginScreen';
-//import RegisterScreen from '../screens/RegisterScreen';
+import SplashScreen from "../screens/SplashScreen";
+import Onboarding1 from "../screens/Onboarding1";
+import Onboarding2 from "../screens/Onboarding2";
+import Onboarding3 from "../screens/Onboarding3";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false, }}>
-        {/*<Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />*/}
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Onboarding1" component={Onboarding1} />
+        <Stack.Screen name="Onboarding2" component={Onboarding2} />
+        <Stack.Screen name="Onboarding3" component={Onboarding3} />
       </Stack.Navigator>
     </NavigationContainer>
-  ); 
+  );
 }
