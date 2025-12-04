0ec4749ea1883c3ce7d86e789992015a94027c24
Likhitha <klikitha907@gmail.com>
2025-12-04 12:38:56 +0530

    Login page code

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Import your screens
 //import HomeScreen from '../screens/HomeScreen';
-//import LoginScreen from '../screens/LoginScreen';
+import LoginScreen from '../screens/LoginScreen';
 //import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
@@ -13,8 +13,8 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false, }}>
-        {/*<Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/*<Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />*/}
       </Stack.Navigator>
     </NavigationContainer>
