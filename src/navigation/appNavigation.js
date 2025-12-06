ec83d3d26974e2e398847798a3c7855bb1762c88
himabindu729 <kodalihimabindu297@gmail.com>
2025-12-06 12:49:46 +0530

    Updated HomeScreen UI and ServiceBlock card styles

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -1,22 +1,33 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import BottomNavigation from "./navigation/BottomNavigation";
+
+// // Import your screens
+// import HomeScreen from '../screens/HomeScreen';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function AppNavigator() {
+//   return (
+//     <NavigationContainer>
+//        <Stack.Navigator>
+        
+//         <Stack.Screen name="Home" component={HomeScreen}  options={{ headerShown: false }}/>
+//         <BottomNavigation />
+//       </Stack.Navigator> 
+//     </NavigationContainer>
+//   );
+// }
+  // src/navigation/appNavigation.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomNavigation from './bottomNavigation'; // same folder
 
-// Import your screens
-//import HomeScreen from '../screens/HomeScreen';
-//import LoginScreen from '../screens/LoginScreen';
-//import RegisterScreen from '../screens/RegisterScreen';
-
-const Stack = createNativeStackNavigator();
-
-export default function AppNavigator() {
+export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false, }}>
-        {/*<Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />*/}
-      </Stack.Navigator>
+      <BottomNavigation />
     </NavigationContainer>
-  ); 
+  );
 }
