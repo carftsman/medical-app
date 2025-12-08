c01f28171b87fb31b7bb4eb1eecd7722b4275f0c
akhila patnala <akhilapatnala@gmail.com>
2025-12-08 10:34:33 +0400

    Done Forgot Password

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -7,12 +7,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 //import LoginScreen from '../screens/LoginScreen';
 //import RegisterScreen from '../screens/RegisterScreen';
 
+
+// src/navigation/appNavigation.js
+import RequestOTPScreen     from '../screens/ForgotPassword/RequestOTPScreen';
+import EnterOTPScreen       from '../screens/ForgotPassword/EnterOTPScreen';
+import ResetPasswordScreen  from '../screens/ForgotPassword/ResetPasswordScreen';
+
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false, }}>
+      <Stack.Navigator initialRouteName="RequestOTP" screenOptions={{headerShown: false, }}>
+        <Stack.Screen name="RequestOTP" component={RequestOTPScreen} />   
+        <Stack.Screen name="EnterOTP" component={EnterOTPScreen} />
+        <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
         {/*<Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />*/}
