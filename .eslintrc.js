91154fb5d200029291676e47b309261707150bc0
harinaikm <harinaik.m@dhatvibs.com>
2026-01-07 14:14:22 +0530

    modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ['module:@react-native/babel-preset'],
-    },
-  },
 };
