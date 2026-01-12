9e7445e107ce8bfa346397910d59c6724e31a89f
Likhitha <klikitha907@gmail.com>
2026-01-12 16:42:59 +0530

    add colors

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,10 +1,23 @@
 export const COLORS = {
-  primary: "#056FD2", // main green shade
+  primary: "#056FD2",
   white: "#ffffff",
   black: "#000000",
   gray: "#7f8c8d",
   lightGray: "#ecf0f1",
   danger: "#e74c3c",
+  pink :"#F6339A",
+  blue :"#2B7FFF",
+  lightblue:"#51A2FF",
+  skyblue:"#00D5BE",
+  purple :"#AD46FF",
+  green:"#00C950",
+  lightGreen:"#0AA30A",
+  darkgray:"#292D32",
+  darkpurple :"#A24FD600",
+  Iceblue :"#DBEAFE",
+  seablue:"#67B6FF"
+
+
 };
 
 export const FONT = {
