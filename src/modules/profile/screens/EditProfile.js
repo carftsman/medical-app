604d308aad57b61a62e06bf110b9f7732b49df6f
Harinaik Meravath <harinaik.m@dhatvibs.com>
2026-02-16 12:04:13 +0530

    profile initial setup

---
+++ src/modules/profile/screens/EditProfile.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const EditProfile = () => {
+  return (
+    <View>
+      <Text>Edit Profile Screen</Text>
+    </View>
+  );
+};
+
+export default EditProfile;
