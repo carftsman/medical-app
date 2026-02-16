c4a4039d36ef9f3fb13e53013dc2d59bae4c09ce
Harinaik Meravath <harinaik.m@dhatvibs.com>
2026-02-16 12:51:51 +0530

    add profile folder structure

--- src/modules/profile/screens/ProfileScreen.js
+++ src/modules/profile/screens/ProfileScreen.js
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const ProfileScreen = () => {
+  const navigation = useNavigation()
   return (
     <View>
       <Text>ProfileScreen</Text>
+      <Button title='Edit Profile' onPress={() => navigation.navigate('EditProfile')} />
     </View>
   )
 }
